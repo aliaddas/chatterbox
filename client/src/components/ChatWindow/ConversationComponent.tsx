cd3a9c84ee29eab9aa41a998405e429f6eeca698
Ali Addas <ali.elhabiladdas@student.odisee.be>
2025-12-02 01:00:25 +0100

    sync cleanup

--- client/src/components/ChatWindow/ConversationComponent.tsx
+++ client/src/components/ChatWindow/ConversationComponent.tsx
@@ -1,20 +1,22 @@
-import { useEffect, useState, useContext, useRef } from "react";
+import {useEffect, useState, useContext, useRef} from "react";
 import ChatBubbleComponent from "./ChatComponents/ChatBubbleComponent";
-import WebSocketContext from '../../context/WebSocketContext'; // import the context
-import SystemMessage from './ChatComponents/SystemMessageComponent';
+import WebSocketContext from "../../context/WebSocketContext"; // import the context
+import SystemMessage from "./ChatComponents/SystemMessageComponent";
 
-import './css/ConversationComponent.css';
-import useUserContext from '../../hooks/useUserContext';
-import { Room } from "../../../../server/contract";
+import "./css/ConversationComponent.css";
+import useUserContext from "../../hooks/useUserContext";
+import {Room} from "../../../../server/contract";
 
 function ConversationComponent() {
-  const [roomMessages, setRoomMessages] = useState<{ [key: string]: CombinedMessages[] }>({}); // set the state
-  const { webSocket, send } = useContext(WebSocketContext); // use the context
+  const [roomMessages, setRoomMessages] = useState<{
+    [key: string]: CombinedMessages[];
+  }>({}); // set the state
+  const {webSocket, send} = useContext(WebSocketContext); // use the context
   const containerRef = useRef<HTMLDivElement>(null);
   const containerElement = containerRef.current;
-  const { username } = useUserContext();
-  const { roomName, setRoomName } = useUserContext();
-  const { rooms, setRooms } = useUserContext();
+  const {username} = useUserContext();
+  const {roomName, setRoomName} = useUserContext();
+  const {rooms, setRooms} = useUserContext();
   const [roomID, setRoomID] = useState<string>("general");
 
   const currentRoomMessages = roomMessages[roomID] || [];
@@ -38,8 +40,9 @@ function ConversationComponent() {
       const formattedRooms: Room[] = fetchedRooms.map((room: any) => {
         return {
           ID: room.id,
-          name: room.name
-        }});
+          name: room.name,
+        };
+      });
       setRooms(formattedRooms);
     } catch (error) {
       console.error("Error fetching rooms:", error);
@@ -49,13 +52,12 @@ function ConversationComponent() {
   console.log("Mounted Rooms:", rooms);
   console.log("Selected Room:", roomName);
   useEffect(() => {
-  fetchRooms();
-}, []);
+    fetchRooms();
+  }, []);
 
   useEffect(() => {
     //! No Websocket found
     if (!webSocket) return;
-
 
     //#
     //# Message Handler
@@ -64,18 +66,17 @@ function ConversationComponent() {
       const data = JSON.parse(event.data);
 
       if (data.action === "userJoinedRoom") {
-
         //? Create a new room object for checks
         const parseRoom: Room = {
           ID: data.payload.roomName,
-          name: data.payload.roomName
+          name: data.payload.roomName,
         };
 
         //# Map names to array
         const checkingRooms = rooms.map((room) => room.name);
 
         //! Does not include name
-        if(!checkingRooms.includes(parseRoom.name)){
+        if (!checkingRooms.includes(parseRoom.name)) {
           console.log("Room doesn't exist in array yet");
           //# Append to rooms
           setRooms([...rooms, parseRoom]);
@@ -83,13 +84,13 @@ function ConversationComponent() {
 
         //# Set the room name
         //*(general by default sent from server)
-        if(parseRoom.name === "general"){
-          setRoomName(parseRoom.name)
+        if (parseRoom.name === "general") {
+          setRoomName(parseRoom.name);
         }
       }
 
       if (data.info === "userConnected") {
-        console.log(data.payload)
+        console.log(data.payload);
 
         // const { roomID, ...message } = data.payload;
 
@@ -113,11 +114,14 @@ function ConversationComponent() {
       }
 
       if (data.action === "newChat") {
-        console.log(data.payload)
+        console.log(data.payload);
         setRoomID(data.payload.roomID);
-        const newMessage = { ...data.payload.newChat, createdAt: new Date(data.payload.newChat.createdAt) };
+        const newMessage = {
+          ...data.payload.newChat,
+          createdAt: new Date(data.payload.newChat.createdAt),
+        };
 
-        setRoomMessages(prevState => {
+        setRoomMessages((prevState) => {
           // If room doesn't exist yet, create it
           if (!prevState[roomID]) {
             prevState[roomID] = [];
@@ -127,10 +131,10 @@ function ConversationComponent() {
           prevState[roomID].push(newMessage);
 
           // Return new state
-          return { ...prevState };
+          return {...prevState};
         });
 
-        console.log(roomMessages)
+        console.log(roomMessages);
       }
     };
 
@@ -139,38 +143,42 @@ function ConversationComponent() {
     };
 
     //@ Event Listeners
-    webSocket.addEventListener('message', messageHandler);
-    webSocket.addEventListener('error', errorHandler);
+    webSocket.addEventListener("message", messageHandler);
+    webSocket.addEventListener("error", errorHandler);
 
     return () => {
       if (webSocket) {
-        webSocket.removeEventListener('message', messageHandler);
-        webSocket.removeEventListener('error', errorHandler);
+        webSocket.removeEventListener("message", messageHandler);
+        webSocket.removeEventListener("error", errorHandler);
       }
     };
-
-  },
-  [
+  }, [
     webSocket,
     send,
     setRoomMessages,
     containerElement,
     roomMessages,
-    username
+    username,
   ]);
 
   return (
     <div ref={containerRef} className="flex flex-grow  overflow-y-auto h-full">
-        <div className="w-20" />
-        <div className="flex-grow h-full pb-5">
-          {currentRoomMessages.map((message: CombinedMessages) => (
-            isSystemMessage(message)
-              ? <SystemMessage key={message.type} type={message.type} message={message.payload.newChat}/>
-              : <ChatBubbleComponent key={message.ID} message={message} />
-          ))}
-        </div>
-        <div className="w-5" />
+      <div className="w-20" />
+      <div className="flex-grow h-full pb-5">
+        {currentRoomMessages.map((message: CombinedMessages) =>
+          isSystemMessage(message) ? (
+            <SystemMessage
+              key={message.type}
+              type={message.type}
+              message={message.payload.newChat}
+            />
+          ) : (
+            <ChatBubbleComponent key={message.ID} message={message} />
+          )
+        )}
       </div>
+      <div className="w-5" />
+    </div>
   );
 }
 
@@ -186,9 +194,9 @@ type ChatMessage = {
 };
 
 type SystemMessage = {
-  type: string
-  payload: any
-}
+  type: string;
+  payload: any;
+};
 
 function isSystemMessage(message: CombinedMessages): message is SystemMessage {
   return (message as SystemMessage).type !== undefined;
